e32abf040c544852ee72efbd6411145f9c5de804
Amanda Treutler <amandathedev@gmail.com>
2019-10-31 13:44:35 -0500

    colors

--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -3,52 +3,77 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { Typography } from "@material-ui/core";
+import classNames from "classnames";
 
 const useStyles = makeStyles(theme => ({
   button: {
-    margin: theme.spacing(1.2)
+    margin: theme.spacing(1.3)
   },
-  buttonTemp: {
-    backgroundColor: "#ED8A68"
-  },
-  buttonLength: {
-    backgroundColor: "#5FA052"
-  },
-  buttonWeight: {
-    backgroundColor: "#F6E481"
+  categoryDiv: {
+    backgroundColor: "#F5F5F5"
   }
 }));
 
+const styles = {
+  buttonTemp: {
+    backgroundColor: "#ff474c"
+  },
+  buttonLength: {
+    backgroundColor: "#3eaf76"
+  },
+  buttonWeight: {
+    backgroundColor: "#F6E481"
+  },
+  buttonSpeed: {
+    backgroundColor: "#448ee4"
+  },
+  buttonCook: {
+    backgroundColor: "#dfc5fe"
+  }
+};
+
 const CategorySelector = () => {
   const classes = useStyles();
+  let tempClasses = classNames("button", "buttonTemp");
 
   return (
-    <div>
+    <div className={classes.categoryDiv}>
       <Typography variant="h4">
         UnitSwitch<i class="fas fa-sync"></i>
       </Typography>
       <Button
         variant="contained"
-        className={(classes.button, classes.buttonTemp)}
+        className={classes.button}
+        style={styles.buttonTemp}
       >
         Temperature
       </Button>
       <Button
         variant="contained"
-        className={(classes.button, classes.buttonLength)}
+        className={classes.button}
+        style={styles.buttonLength}
       >
         Length
       </Button>
       <Button
         variant="contained"
-        className={(classes.button, classes.buttonWeight)}
+        className={classes.button}
+        style={styles.buttonWeight}
       >
         Weight
       </Button>
-      <Button variant="contained" className={classes.button}>
+      <Button
+        variant="contained"
+        className={classes.button}
+        style={styles.buttonSpeed}
+      >
         Speed
       </Button>
-      <Button variant="contained" className={classes.button}>
+      <Button
+        variant="contained"
+        className={classes.button}
+        style={styles.buttonCook}
+      >
         Cooking
       </Button>
     </div>
