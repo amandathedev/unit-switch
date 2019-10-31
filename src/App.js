71c6bfd74b5703237a493e57779646efa554f411
Amanda Treutler <amandathedev@gmail.com>
2019-10-31 10:41:31 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select class="value-dropdown" name="units">
+        <option value="temperature">Temperature</option>
+        <option value="weight">Weight</option>
+        <option value="distance">Distance</option>
+      </select>
+      <form>
+        <span>Fahrenheit</span>
+        <input class="input-box" type="text" />
+        <button type="button">
+          Convert <i class="fas fa-arrow-down"></i>
+        </button>
+        <hr />
+        <span>Celcius</span>
+        <input class="input-box" type="text" />
+        <button type="button">
+          Convert <i class="fas fa-arrow-up"></i>
+        </button>
+      </form>
     </div>
   );
 }
