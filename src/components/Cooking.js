0d5c2b4b50fe3363c2d3b48ec680e12e35ddd061
Amanda Treutler <amandathedev@gmail.com>
2019-11-05 09:18:17 -0600

    margin

--- src/components/Cooking.js
+++ src/components/Cooking.js
@@ -74,8 +74,8 @@ const styles = {
   },
   backButton: {
     backgroundColor: "#282828",
-    color: "#f9f2f9"
-    // marinBottom: "1em"
+    color: "#f9f2f9",
+    marinBottom: "1em"
     // paddingTop: "0",
     // paddingBottom: "5em"
   }
